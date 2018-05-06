683ec580628766856b060e6d27f1f2bfe2efa252
Fluzko <37568902+Fluzko@users.noreply.github.com>
2018-05-06 16:04:31 -0300

    Update and rename README.md to README.hs

---
+++ README.hs
@@ -0,0 +1,16 @@
+# tpParadigmas
+
+programCounter :: Microcontrolador -> Int
+programCounter microcontrolador = programCounter microcontrolador 
+
+acumuladorA :: Microcontrolador -> Int
+acumuladorA microcontrolador =  acumuladorA microcontrolador
+
+acumuladorB :: Microcontrolador -> Int
+acumuladorB microcontrolador = acumuladorB microcontrolador
+
+memoria :: Microcontrolador -> Int
+memoria microcontrolador = length (memoriaDeDatos microcontrolador)
+
+mensajeError :: Microcontrolador -> String
+mensajeError microcontrolador = ultimoErrorProducido microcontrolador
