f090448b4f1a356048297e1e5cc1dc2bcbe2f2ab
Fluzko <37568902+Fluzko@users.noreply.github.com>
2018-05-06 16:04:52 -0300

    Update README.hs

--- README.hs
+++ README.hs
@@ -1,16 +1,2 @@
 # tpParadigmas
 
-programCounter :: Microcontrolador -> Int
-programCounter microcontrolador = programCounter microcontrolador 
-
-acumuladorA :: Microcontrolador -> Int
-acumuladorA microcontrolador =  acumuladorA microcontrolador
-
-acumuladorB :: Microcontrolador -> Int
-acumuladorB microcontrolador = acumuladorB microcontrolador
-
-memoria :: Microcontrolador -> Int
-memoria microcontrolador = length (memoriaDeDatos microcontrolador)
-
-mensajeError :: Microcontrolador -> String
-mensajeError microcontrolador = ultimoErrorProducido microcontrolador
