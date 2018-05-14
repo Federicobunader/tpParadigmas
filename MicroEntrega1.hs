7f207eecac6fdd60e947c49219ef583b3fcc38a6
Fluzko <37568902+Fluzko@users.noreply.github.com>
2018-05-14 15:10:11 -0300

    Add files via upload

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -1,53 +1,34 @@
 module MicroEntrega1 where
-
 type Posicion = Int
 data Microcontrolador = Microcontrolador {memoriaDeDatos :: [Posicion], acumA :: Int, acumB :: Int, contadorPrograma :: Int, ultimoErrorProducido :: String} deriving (Show, Eq)
-
-xt8088 = Microcontrolador {memoriaDeDatos = replicate 1024 0, acumA = 0, acumB = 0, contadorPrograma = 0, ultimoErrorProducido = ""}
+xt8088 = Microcontrolador {memoriaDeDatos = replicate 1024 0, acumA = 2, acumB = 4, contadorPrograma = 0, ultimoErrorProducido = ""}
 fp20 = Microcontrolador {memoriaDeDatos = replicate 1024 0, acumA = 7, acumB = 24, contadorPrograma = 0, ultimoErrorProducido = ""}
 at8086 = Microcontrolador {memoriaDeDatos = [1..20], acumA = 0, acumB = 0, contadorPrograma = 0, ultimoErrorProducido = ""}
-
 nop :: Microcontrolador -> Microcontrolador
-nop microcontrolador = microcontrolador {contadorPrograma = contadorPrograma microcontrolador + 1}
-
+nop microcontrolador = aumentarContador microcontrolador
 lodv :: Int -> Microcontrolador -> Microcontrolador
-lodv val microcontrolador = microcontrolador {acumA = val, contadorPrograma = contadorPrograma microcontrolador + 1}
-
+lodv val microcontrolador = aumentarContador microcontrolador {acumA = val}
 swap :: Microcontrolador -> Microcontrolador
-swap microcontrolador = microcontrolador {acumA = acumB microcontrolador, acumB = acumA microcontrolador, contadorPrograma = contadorPrograma microcontrolador + 1}
-
+swap microcontrolador = aumentarContador microcontrolador {acumA = acumB microcontrolador, acumB = acumA microcontrolador}
+swap2 :: Microcontrolador -> Microcontrolador
+swap2 (Microcontrolador memoriaDeDatos acumA acumB contadorPrograma ultimoErrorProducido) = aumentarContador (Microcontrolador memoriaDeDatos acumB acumA contadorPrograma ultimoErrorProducido)
 add :: Microcontrolador -> Microcontrolador
-add microcontrolador = microcontrolador {acumA = acumA microcontrolador + acumB microcontrolador, acumB = 0, contadorPrograma = contadorPrograma microcontrolador + 1}
-
+add microcontrolador = aumentarContador microcontrolador {acumA = acumA microcontrolador + acumB microcontrolador, acumB = 0}
 divide :: Microcontrolador-> Microcontrolador
-divide microcontrolador | acumB microcontrolador == 0 = microcontrolador{ultimoErrorProducido = "DIVISION BY ZERO", contadorPrograma = contadorPrograma microcontrolador + 1}
-                        | otherwise = microcontrolador {acumA = acumA microcontrolador `div` acumB microcontrolador, acumB = 0, contadorPrograma = contadorPrograma microcontrolador + 1}
-
+divide microcontrolador | acumB microcontrolador == 0 = aumentarContador microcontrolador {ultimoErrorProducido = "DIVISION BY ZERO"}
+                        | otherwise = aumentarContador microcontrolador {acumA = div (acumA microcontrolador) (acumB microcontrolador), acumB = 0}
 str :: Posicion -> Int -> Microcontrolador -> Microcontrolador
-str addr val microcontrolador = microcontrolador { memoriaDeDatos = insertarEnMemoria addr val (memoriaDeDatos microcontrolador), contadorPrograma = contadorPrograma microcontrolador + 1}
-
+str addr val microcontrolador = aumentarContador microcontrolador { memoriaDeDatos = insertarEnMemoria addr val (memoriaDeDatos microcontrolador)}
 insertarEnMemoria :: Posicion -> Int -> [Posicion] -> [Posicion]
 insertarEnMemoria addr val lista = (take (addr-1) lista) ++ [val] ++ (drop addr lista)
-
 lod :: Posicion -> Microcontrolador -> Microcontrolador
-lod addr microcontrolador = microcontrolador { acumA = (memoriaDeDatos microcontrolador) !! addr, contadorPrograma = contadorPrograma microcontrolador + 1}
+lod addr microcontrolador = aumentarContador microcontrolador { acumA = (memoriaDeDatos microcontrolador) !! addr}
+aumentarContador :: Microcontrolador -> Microcontrolador
+aumentarContador microcontrolador = microcontrolador {contadorPrograma = contadorPrograma microcontrolador + 1}
 
--- FUNCIONES PEDIDAS PARA LA ENTREGA:
 
-programCounter :: Microcontrolador -> Int
-programCounter microcontrolador = contadorPrograma microcontrolador 
 
-acumuladorA :: Microcontrolador -> Int
-acumuladorA microcontrolador =  acumA microcontrolador
 
-acumuladorB :: Microcontrolador -> Int
-acumuladorB microcontrolador = acumB microcontrolador
-
-memoria :: Microcontrolador -> Int
-memoria microcontrolador = length (memoriaDeDatos microcontrolador)
-
-mensajeError :: Microcontrolador -> String
-mensajeError microcontrolador = ultimoErrorProducido microcontrolador
 
 
 
